495afa45437423628dba36c2cd4240cf56fa578f
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-03-21 18:52:26 +0530

    create js folder and add student data

---
+++ frontend-assignment/js/GarvSinghChouhan_student_analyzer.js
@@ -0,0 +1,47 @@
+﻿const students = [
+  {
+    name: "Lalit",
+    marks: [
+      { subject: "Math", score: 78 },
+      { subject: "English", score: 82 },
+      { subject: "Science", score: 74 },
+      { subject: "History", score: 69 },
+      { subject: "Computer", score: 88 }
+    ],
+    attendance: 82
+  },
+  {
+    name: "Rahul",
+    marks: [
+      { subject: "Math", score: 90 },
+      { subject: "English", score: 85 },
+      { subject: "Science", score: 80 },
+      { subject: "History", score: 76 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 91
+  },
+  {
+    name: "Aman",
+    marks: [
+      { subject: "Math", score: 86 },
+      { subject: "English", score: 72 },
+      { subject: "Science", score: 38 },
+      { subject: "History", score: 65 },
+      { subject: "Computer", score: 79 }
+    ],
+    attendance: 88
+  },
+  {
+    name: "Riya",
+    marks: [
+      { subject: "Math", score: 67 },
+      { subject: "English", score: 74 },
+      { subject: "Science", score: 71 },
+      { subject: "History", score: 70 },
+      { subject: "Computer", score: 76 }
+    ],
+    attendance: 68
+  }
+];
+
