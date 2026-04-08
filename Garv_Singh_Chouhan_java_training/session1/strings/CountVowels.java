5a59f26e06d012159eabe4965c5368f00440bffb
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-04-08 23:44:20 +0530

    implemented strings, countvowels and reverse functions/questions

---
+++ Garv_Singh_Chouhan_java_training/session1/strings/CountVowels.java
@@ -0,0 +1,16 @@
+﻿package session1.strings;
+
+public class CountVowels {
+    public static int countVowels(String str) {
+    int count = 0;
+    for (char c : str.toLowerCase().toCharArray()) {
+        if ("aeiou".indexOf(c) != -1) count++;
+    }
+    return count;
+}
+
+    public static void main(String[] args) {
+        String input = "Hello World";
+        System.out.println("Number of vowels in \"" + input + "\": " + countVowels(input));
+    }
+}
---
+++ Garv_Singh_Chouhan_java_training/session1/strings/Reverse.java
@@ -0,0 +1,12 @@
+﻿package session1.strings;
+// here i have used StringBuilder to reverse the string, which is more efficient than using a loop to concatenate characters in reverse order.
+public class Reverse {
+    public static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println(reverse(str));
+    }
+}
