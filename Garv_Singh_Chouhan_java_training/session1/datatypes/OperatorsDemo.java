85e02a09df811b40185d86d3aa651cbbfe41d1a4
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-04-08 23:11:37 +0530

    implemented operators and temprature questions

---
+++ Garv_Singh_Chouhan_java_training/session1/datatypes/OperatorsDemo.java
@@ -0,0 +1,17 @@
+﻿package session1.datatypes;
+
+public class OperatorsDemo {
+
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        // Arithmetic operators
+        System.out.println(a + b);
+
+        // Relational operators
+        System.out.println(a > b);
+
+        // Logical operators
+        System.out.println(a > 5 && b < 10);
+    }
+}
