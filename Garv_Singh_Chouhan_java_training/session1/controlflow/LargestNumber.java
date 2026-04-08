fa11477b82b6f20d6eb025485236d122af52c452
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-04-08 23:22:32 +0530

    implemented controlflow functions

---
+++ Garv_Singh_Chouhan_java_training/session1/controlflow/LargestNumber.java
@@ -0,0 +1,10 @@
+﻿package session1.controlflow;
+
+public class LargestNumber {
+
+    public static int findLargest(int a, int b, int c) {
+        if (a >= b && a >= c) return a;
+        else if (b >= c) return b;
+        else return c;
+    }
+}
---
+++ Garv_Singh_Chouhan_java_training/session1/controlflow/MultiplicationTable.java
@@ -0,0 +1,14 @@
+﻿package session1.controlflow;
+
+public class MultiplicationTable {
+
+    public static void printMultiplicationTable(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
+
+
+
+
