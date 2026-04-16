6926ebf408fe6655d75e8ad245f2b180adaca48a
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-04-16 21:58:33 +0530

    restored everything

---
+++ Garv_Singh_Chouhan_java_training/session1/oop/Student.java
@@ -0,0 +1,19 @@
+﻿package session1.oop;
+
+public class Student {
+
+    // Encapsulation (private variables)
+    private String name;
+    private int rollNo;
+    private double marks;
+
+    public Student(String name, int rollNo, double marks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
+
+    public void display() {
+        System.out.println(name + " " + rollNo + " " + marks);
+    }
+}
