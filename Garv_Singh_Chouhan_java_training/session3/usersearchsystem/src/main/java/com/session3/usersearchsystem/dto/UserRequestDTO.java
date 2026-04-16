ebeb39a0ef0546553fe9b23739aa7cd7f5c4764d
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-04-17 00:44:29 +0530

    implemented userRequest dto

---
+++ Garv_Singh_Chouhan_java_training/session3/usersearchsystem/src/main/java/com/session3/usersearchsystem/dto/UserRequestDTO.java
@@ -0,0 +1,12 @@
+package com.session3.usersearchsystem.dto;
+
+public class UserRequestDTO {
+
+    private String name;
+    private Integer age;
+    private String role;
+
+    public String getName() { return name; }
+    public Integer getAge() { return age; }
+    public String getRole() { return role; }
+}
