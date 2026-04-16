1c83c94db5933892933ded554433051608e595ca
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-04-17 01:12:59 +0530

    implemented userController

---
+++ Garv_Singh_Chouhan_java_training/session3/usersearchsystem/src/main/java/com/session3/usersearchsystem/controller/UserController.kt
@@ -0,0 +1,49 @@
+package com.session3.usersearchsystem.controller
+
+import com.session3.usersearchsystem.dto.UserRequestDTO
+import com.session3.usersearchsystem.service.UserService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+
+// Controller layer handles API requests
+// No business logic should be written here
+@RestController
+@RequestMapping("/users")
+class UserController // Constructor injection (MANDATORY as per assignment)
+    (private val userService: UserService) {
+    // SEARCH API
+    // If params are null → returns all users
+    @GetMapping("/search")
+    fun searchUsers(
+        @RequestParam(required = false) name: String?,
+        @RequestParam(required = false) age: Int?,
+        @RequestParam(required = false) role: String?
+    ): MutableList<User?> {
+        return userService.searchUsers(name, age, role)
+    }
+
+    // SUBMIT API
+    @PostMapping("/submit")
+    fun submitUser(@RequestBody dto: UserRequestDTO?): ResponseEntity<*> {
+        try {
+            val user: User? = userService.submitUser(dto)
+
+            // returning 201 CREATED
+            return ResponseEntity.status(201).body<Any?>(user)
+        } catch (e: IllegalArgumentException) {
+            // returning 400 BAD REQUEST
+
+            return ResponseEntity.badRequest().body<String?>(e.message)
+        }
+    }
+
+    // DELETE API with confirmation
+    @DeleteMapping("/{id}")
+    fun deleteUser(
+        @PathVariable id: Long?,
+        @RequestParam(required = false) confirm: Boolean?
+    ): String {
+        return userService.deleteUser(id, confirm)
+    }
+}
