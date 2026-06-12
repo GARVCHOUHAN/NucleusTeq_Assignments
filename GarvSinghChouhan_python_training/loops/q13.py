5fccbccff84c562e98b78e3882cb3ed8a0ef9f89
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-06-12 23:10:59 +0530

    added loops questions starting from question 12 to 16

---
+++ GarvSinghChouhan_python_training/loops/q13.py
@@ -0,0 +1,15 @@
+﻿""" Print the multiplication table of a number """
+
+
+def print_multiplication_table() -> None:
+    """
+    Print the multiplication table of a number entered by user
+    """
+    num = int(input("Enter a number: "))
+
+    for count in range(1, 11):
+        print(f"{num} x {count} = {num * count}")
+
+
+if __name__ == "__main__":
+    print_multiplication_table()
---
+++ GarvSinghChouhan_python_training/loops/q16.py
@@ -0,0 +1,23 @@
+﻿""" Check whether a number is prime """
+
+
+def check_prime_number() -> None:
+    """
+    Check if a number is prime or not
+    """
+    number = int(input("Enter a number: "))
+
+    if number <= 1:
+        print(f"{number} is not a prime number")
+        return
+
+    # prime number is divisible only by 1 and itself
+    for i in range(2, number):
+        if number % i == 0:
+            print("not a prime number")
+            return
+    print("prime number")
+
+
+if __name__ == "__main__":
+    check_prime_number()
