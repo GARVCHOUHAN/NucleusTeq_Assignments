45229030a908652d23f7a2a48c63453b30e37a3a
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-06-12 23:21:32 +0530

    implemented and added functions from question 17 to question 20

---
+++ GarvSinghChouhan_python_training/functions/q20.py
@@ -0,0 +1,11 @@
+﻿""" a function using default parameters."""
+
+def default_msg(name:str = "User") -> None:
+    """
+    Display a greeting message.
+    """
+    print(f"Welcome, {name}!")
+
+if __name__ == "__main__":
+    default_msg()  # Using default parameter
+    default_msg("Neymar Jr")  # Providing a custom name
