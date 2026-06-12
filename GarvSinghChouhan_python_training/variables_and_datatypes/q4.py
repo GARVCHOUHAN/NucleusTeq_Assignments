9ff88c9ea0c02b572aecb5e9dac0f4c70206a83d
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-06-12 22:32:43 +0530

    added variables and datatypes questions

---
+++ GarvSinghChouhan_python_training/variables_and_datatypes/q4.py
@@ -0,0 +1,23 @@
+﻿""" 
+Create variables of type int, float, string and boolean
+Print their types using type() 
+"""
+
+def print_variable_types() -> None: 
+    """                                                         
+    Create variables and display their data types
+    """
+    integer_value = 100
+    float_value = 12.45
+    string_value = "Garv Singh Chouhan"
+    boolean_value = False
+
+    # displaying data types of variable 
+    print(type(integer_value))
+    print(type(float_value))
+    print(type(string_value))
+    print(type(boolean_value))
+
+
+if __name__ == "__main__":
+    print_variable_types()
