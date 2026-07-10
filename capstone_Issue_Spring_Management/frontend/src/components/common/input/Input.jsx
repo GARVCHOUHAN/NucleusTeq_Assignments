c729422205ab4afbd8d412614895260bab3aebc4
Garv Singh Chouhan <garvchouhan5@gmail.com>
2026-07-10 22:55:54 +0530

    added UI enhancements

---
+++ capstone_Issue_Spring_Management/frontend/src/components/common/input/Input.jsx
@@ -0,0 +1,34 @@
+﻿import styles from "./Input.module.css";
+
+
+function Input({
+    label,
+    type,
+    value,
+    onChange,
+    placeholder
+}) {
+
+    return (
+
+        <div className={styles.inputGroup}>
+
+            <label>
+                {label}
+            </label>
+
+            <input
+                type={type}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+            />
+
+        </div>
+
+    );
+
+}
+
+
+export default Input;
---
+++ capstone_Issue_Spring_Management/frontend/src/components/common/sidebar/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+import { ROUTES } from "../../../constants/routes";
+import styles from "./Sidebar.module.css";
+
+
+function Sidebar(){
+const linkClass=({isActive})=>isActive ? styles.active : undefined;
+
+return(
+<aside className={styles.sidebar}>
+
+<h2>IMS</h2>
+
+<nav className={styles.nav}>
+<NavLink className={linkClass} to={ROUTES.DASHBOARD}>Dashboard</NavLink>
+<NavLink className={linkClass} to={ROUTES.PROJECTS}>Projects</NavLink>
+<NavLink className={linkClass} to={ROUTES.ISSUES}>Issues</NavLink>
+<NavLink className={linkClass} to={ROUTES.SPRINTS}>Sprints</NavLink>
+</nav>
+
+</aside>
+);
+}
+
+export default Sidebar;
